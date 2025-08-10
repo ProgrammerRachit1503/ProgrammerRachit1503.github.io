5e2b1425e85918de5cf9f05d14fc73c9abc78575
TechGamer <rachitjain75.rj75@gmail.com>
2025-08-11 00:59:46 +0530

    Replace site with new template

--- JavaScripts/Typing Animate.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed('#element', {
-    strings: ['A Passionate Coder &amp; Learner', 'Python Developer', 'Lazy Developer'],
-    typeSpeed: 50,
-    backDelay: 500,
-    backSpeed: 20,
-    loop: true
-});
